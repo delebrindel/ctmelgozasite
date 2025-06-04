ca1304bd399b80e77f2d1c26915345bf3c66609d
Cristobal Torres <ctmelgoza@gmail.com>
2025-06-03 22:37:47 -0700

    FEAT - Portfolio

---
+++ src/Components/Portfolio/Portfolio.Component.tsx
@@ -0,0 +1,8 @@
+export const Portfolio = ()=> {
+  return (
+    <div className="portfolio">
+      <h1>Portfolio</h1>
+      <p>Work in progress! Come back soon</p>
+    </div>
+  );
+}
--- src/Components/UI/Router.Component.tsx
+++ src/Components/UI/Router.Component.tsx
@@ -2,13 +2,14 @@ import { useAppStore } from "../../Global/App.Store";
 import { ROUTES } from "../../Global/Constants.Enum";
 import { About } from "../About/About.Component";
 import { CV } from "../CV/CV.Component";
+import { Portfolio } from "../Portfolio/Portfolio.Component";
 
 export const Router = () => {
   const { currentRoute } = useAppStore();
   return (
     <div className="my-15 ">
       {currentRoute === ROUTES.ABOUT && <About />}
-      {currentRoute === ROUTES.PORTFOLIO && <CV />}
+      {currentRoute === ROUTES.PORTFOLIO && <Portfolio/>}
       {currentRoute === ROUTES.CV && <CV />}
     </div>
   );
